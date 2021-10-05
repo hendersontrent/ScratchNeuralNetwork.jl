7718c01aa4ea3b0c7f108a842e454ecb90b44330
Trent Henderson <trent.henderson1@outlook.com>
2021-10-05 20:05:24 +1100

    Base architecture

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # neural-network-julia
 Builds some simple architecture and test cases for neural networks from scratch in Julia.
+
+Note that this repository is very much a sandbox and there is no guarantee anything runs; I'm simply using it for my own learning and exploration.
---
+++ test-cases/binary-classification.jl
@@ -0,0 +1,17 @@
+#----------------------------------------
+# This script sets out to test some
+# neural network architectures developed
+# in this project
+#----------------------------------------
+
+#----------------------------------------
+# Author: Trent Henderson, 5 October 2021
+#----------------------------------------
+
+# Simulate some data for classification
+
+
+
+# Run the network
+
+
