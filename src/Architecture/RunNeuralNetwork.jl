b46c1533694d33e832ce89e82a4e598c8220c90e
Trent Henderson <trent.henderson1@outlook.com>
2021-10-06 16:37:16 +1100

    Cleaned up main function

--- src/Architecture/RunNeuralNetwork.jl
+++ src/Architecture/RunNeuralNetwork.jl
@@ -1,28 +1,43 @@
 """
-    RunNeuralNetwork(epochs, NeuralNetwork)
+    RunNeuralNetwork(Epochs, ActivationFunction, LossFunction, NeuralNetwork)
 
 Train a neural network.
 
     Usage:
 ```julia-repl
-RunNeuralNetwork(epochs, NeuralNetwork)
+RunNeuralNetwork(epochs, ActivationFunction, LossFunction, NeuralNetwork)
 ```
 Arguments:
-- `epochs` : The number of iterations to train the network for.
+- `Epochs` : The number of iterations to train the network for.
+- `ActivationFunction` : The activation function to use.
+- `LossFunction` : The loss function to use.
 - `NeuralNetwork` : The NeuralNetwork structure.
 """
-function RunNeuralNetwork(epochs::Int64 = 1000, NeuralNetwork)
+function RunNeuralNetwork(Epochs::Int64 = 1000, ActivationFunction::String = "Sigmoid", LossFunction::String = "MeanSquareError", NeuralNetwork)
+
+    # Check function arguments
+
+    (ActivationFunction == "Sigmoid" || ActivationFunction == "Tanh" || ActivationFunction == "ReLU" || ActivationFunction == "Linear") || error("`ActivationFunction` should be a String of either 'Sigmoid', 'Tanh', 'ReLU' or 'Linear'.")
+
+    (LossFunction == "SumOfSquaresError" || LossFunction == "MeanSquareError" || LossFunction == "CrossEntropy") || error("`LossFunction` should be a String of either 'SumOfSquaresError', 'MeanSquareError', or 'CrossEntropy'.")
 
     # Instantiate empty vector to store loss function results
 
-    loss = zeros(epochs)
+    loss = zeros(Epochs)
 
     # Train the network
 
-    for i in 1:epochs
-        print(string("Running epoch: ", i, " of ", epochs))
+    for i in 1:Epochs
+        print(string("Running epoch: ", i, " of ", Epochs))
         NeuralNetwork = FeedForward(NeuralNetwork)
-        NeuralNetwork = BackPropogation(NeuralNetwork)
-        loss[i] = SumOfSquaresError(NeuralNetwork)
+        NeuralNetwork = BackPropogation(NeuralNetwork, ActivationFunction)
+
+        if LossFunction == "MeanSquareError"
+            loss[i] = MeanSquareError(NeuralNetwork)
+        elseif LossFunction == "SumOfSquaresError"
+            loss[i] = SumOfSquaresError(NeuralNetwork)
+        elseif LossFunction == "CrossEntropy"
+            loss[i] = CrossEntropy(NeuralNetwork)
+        end
     end
 end
